f6aabe96a085a0b5559cbb196269166dcd299e60
Rodolfo Torres <rtorresware@gmail.com>
2023-03-02 09:27:44 -0600

    update nixpkgs to unstable to use latest devenv version

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.11-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     neovim.url = "github:rtorresware/nvim.flake";
     neovim.inputs.nixpkgs.follows = "nixpkgs";
-    devenv.url = "github:cachix/devenv/v0.6";
+    devenv.url = "github:cachix/devenv/latest";
     devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
 
